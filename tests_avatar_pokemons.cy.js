80e79602f0db233444c9351cc793920cb5075628
Xseniyata <kisja.bor@gmail.com>
2025-08-22 08:11:14 +0300

    tests_avatar_pokemons.cy.js

--- tests_avatar_pokemons.cy.js
+++ tests_avatar_pokemons.cy.js
@@ -1,27 +1,27 @@
-describe('Тестирование открытия сайта', function () {
+describe('Проверка открытия сайта', function () {
 
      it('search something', function () {
         cy.visit('https://pokemonbattle.ru/');
         })
 })
-describe('Проверка покупки нового аватара', function () {                 // название набора тестов
-    it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
-         cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
-         cy.get('input[id="k_email"]').type('USER_LOGIN');                   // вводим логин
-         cy.get('input[id="k_password"]').type('USER_PASSWORD');               // вводим пароль
-         cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
+describe('Проверка покупки нового аватара', function () {
+    it('e2e тест на покупку нового аватара для тренера', function () {
+         cy.visit('https://pokemonbattle.ru/');
+         cy.get('input[id="k_email"]').type('USER_LOGIN');
+         cy.get('input[id="k_password"]').type('USER_PASSWORD');
+         cy.get('button[type="submit"]').click();
+         cy.wait(3000);
+         cy.get('.header_card_trainer').click();
          cy.wait(2000);
-         cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
-         cy.wait(2000);
-         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
-         cy.get('.available > button').first().click();   // кликаем Купить у первого доступного аватара
-         cy.get('.card_number').type('4620869113632996');                     // вводим номер карты
-         cy.get('.card_csv').type('125');                             // вводим CVV карты
-         cy.get('.card_date').type('1226');                           // вводим срок действия карты
-         cy.get('.card_name').type('NAME');                           // вводим имя владельца действия карты
-         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();     // нажимаем кнопку Оплатить
-         cy.get('.threeds_number').type('56456');                            // вводим код подтверждения СМС
-         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();   // нажимаем кнопку Оплатить
-         cy.contains('Покупка прошла успешно').should('be.visible');     // проверяем наличие и видимость сообщения об успешной покупке
+         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click();
+         cy.get('.available > button').first().click();
+         cy.get('.card_number').type('4620869113632996');
+         cy.get('.card_csv').type('125');
+         cy.get('.card_date').type('1226');
+         cy.get('.card_name').type('NAME');
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+         cy.get('.threeds_number').type('56456');
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+         cy.contains('Покупка прошла успешно').should('be.visible');
      })
      })
